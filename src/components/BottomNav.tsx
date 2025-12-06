758412342f61f558355ced4ee4beaa032769bad5
skip5this <skb.rsvp@gmail.com>
2025-12-05 17:57:35 -0800

    Add 32px bottom padding to nav bar and fix bg color

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -9,7 +9,7 @@ interface BottomNavProps {
 
 export const BottomNav: React.FC<BottomNavProps> = ({ currentScreen, onNavigate }) => {
     return (
-        <nav className="fixed bottom-0 left-0 right-0 bg-[#242424] border-t border-gray-800 pb-safe">
+        <nav className="fixed bottom-0 left-0 right-0 bg-[#1a1a1a] border-t border-gray-800 pb-8">
             <div className="flex justify-around items-center h-16">
                 <button
                     onClick={() => onNavigate('today')}
