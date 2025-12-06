0d58b272fc14649c6a12d87033dbf2d711ff68a3
skip5this <skb.rsvp@gmail.com>
2025-12-05 17:45:20 -0800

    Update status bar color to match background grey

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         name: 'Daily Progress',
         short_name: 'DailyProgress',
         description: 'Track your daily health metrics',
-        theme_color: '#000000',
+        theme_color: '#242424',
         icons: [
           {
             src: 'pwa-192x192.png',
